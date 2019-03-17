33f5cc22a9a5befb8d324e7594c76fb255541652
Matt <b5010228@my.shu.ac.uk>
2019-03-17 16:23:33 +0000

    added in unique login tokens per constiuency

--- config/enviromentVariables.js
+++ config/enviromentVariables.js
@@ -1,5 +1,6 @@
 
 module.exports = {
-    'secret': 'JEREMEY CORBYN',
+    'localSecret': '7bda69e0-4b3f-4984-8bfc-aa161b3d0440',
+    'globalSecret': 'JEREMEY CORBYN',
     'consistuency': 'North Sheffield'
   };
