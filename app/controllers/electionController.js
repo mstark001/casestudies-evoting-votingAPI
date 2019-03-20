72f8bd9724b441d46bbde274c8017426ec04178e
Matt <b5010228@my.shu.ac.uk>
2019-03-20 20:43:53 +0000

    minor

--- app/controllers/electionController.js
+++ app/controllers/electionController.js
@@ -202,15 +202,15 @@ class ElectionController {
 
             const userId = user._id;
             const userIdObject = { '_id': new ObjectID(userId) };
-            User.findOne(userIdObject, (err, out) => {
-                if (out.submittedVotes == null)
-                    out.submittedVotes = [];
+            //User.findOne(userIdObject, (err, out) => {
+                //if (out.submittedVotes == null)
+                    //out.submittedVotes = [];
 
                 // if (!(out.submittedVotes.includes(electionId))) {
                 //     out.submittedVotes.push(electionId);
                 //     User.updateOne(userIdObject, out.toJSON(), (err, res) => {})
                 // }
-            });
+            //});
 
 
 
